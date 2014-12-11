84533ca564a727d6ac9acea7ac3c204a49b411be
t.hoefler <t.hoefler@141.19.145.77>
2014-12-11 15:29:22 +0100

    kleine Änderungen Enum, Factory

--- src/Crypterzeug/CrypterFactory.java
+++ src/Crypterzeug/CrypterFactory.java
@@ -1,24 +1,25 @@
 package Crypterzeug;
 
-
+import Verschluesselungsverfahren.*;
 
 public class CrypterFactory {
-	
-	
-	
-	
-	public createCrypter()throws IllegalKeyException{
-		
-		
-		
-	}
-	
-	
-	
-	
-	toString(){
-		
-		
-	}
 
+	public Crypter createCrypter(String schluessel, Enum selectedCrypter) {
+
+		switch (selectedCrypter) {
+		case CAESAR:
+			return new CrypterCaesar(schluessel);
+		case NULL:
+			return new CrypterNull();
+		case REVERSE:
+			return new CrypterReverse();
+		case SUBSTITUTION:
+			return new CrypterSubstitution(schluessel);
+		case XOR:
+			return new CrypterXOR(schluessel);
+		default:
+			return null;
+		}
+
+	}
 }
--- src/Crypterzeug/Enum.java
+++ src/Crypterzeug/Enum.java
@@ -2,16 +2,8 @@ package Crypterzeug;
 
 public enum Enum {
 	
-	CAESAR(65,90), SUBSTITUTION(65,90), REVERSE(65,90), XOR(65,90), NULL(65,90);
+	CAESAR(), SUBSTITUTION(), REVERSE(), XOR(), NULL();
 
-	private final int ersterBuchstabe;
-	private final int letzterBuchstabe;
-
-	private Enum(int ersterBuchstabe, int letzterBuchstabe) {
-		this.ersterBuchstabe = ersterBuchstabe;
-		this.letzterBuchstabe = letzterBuchstabe;
-
-		
 	}
 	
-}
+
